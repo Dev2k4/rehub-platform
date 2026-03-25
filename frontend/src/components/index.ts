c647097c72f3033faf145f3fc71c75720fcfb5be
Dev2k4 <daranbull1112@gmail.com>
2026-03-25 07:44:09 +0700

    config main path

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,50 @@
+/**
+ * ReHub Components - Main Export Index
+ *
+ * Usage:
+ * import { Button, Card, ListingCard, MainNav } from "@/components"
+ */
+
+// UI Components (shadcn/ui)
+export * from "./ui/alert"
+export * from "./ui/avatar"
+export * from "./ui/badge"
+export * from "./ui/button"
+export * from "./ui/button-group"
+export * from "./ui/card"
+export * from "./ui/checkbox"
+export * from "./ui/dialog"
+export * from "./ui/dropdown-menu"
+export * from "./ui/form"
+export * from "./ui/input"
+export * from "./ui/label"
+export * from "./ui/loading-button"
+export * from "./ui/pagination"
+export * from "./ui/password-input"
+export * from "./ui/select"
+export * from "./ui/separator"
+export * from "./ui/sheet"
+export * from "./ui/sidebar"
+export * from "./ui/skeleton"
+export * from "./ui/sonner"
+export * from "./ui/table"
+export * from "./ui/tabs"
+export * from "./ui/tooltip"
+
+// Common Components
+export * from "./common"
+
+// Composite Components
+export * from "./composite"
+
+// Layout Components
+export * from "./layout"
+
+// Feedback Components (Loading, Error, Empty states)
+export * from "./feedback"
+
+// Form Components
+export * from "./forms"
+
+// Layouts (Page layouts)
+export * from "./layouts"
